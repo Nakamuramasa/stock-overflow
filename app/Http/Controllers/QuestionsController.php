5a7c2994f94bbf4b7f06941284c42a0e037d12a9
Nakamuramasa <ebrten14@gmail.com>
2020-11-07 22:01:29 +0900

    Implement authorization using Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,11 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -61,7 +66,7 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        if(\Gate::denies('update-question', $question)) abort(403, "Access denied");
+        $this->authorize("update", $question);
         return view("questions.edit", compact('question'));
     }
 
@@ -74,7 +79,7 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
-        if(\Gate::denies('update-question', $question)) abort(403, "Access denied");
+        $this->authorize("update", $question);
         $question->update($request->only('title', 'body'));
         return redirect('/questions')->with('success', 'Your question has been updated.');
     }
@@ -87,8 +92,7 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        if(\Gate::denies('delete-question', $question)) abort(403, "Access denied");
-
+        $this->authorize("delete", $question);
         $question->delete();
         return redirect('/questions')->with('success', "Your question has been deleted.");
     }
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Question;
+use App\Policies\QuestionPolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+        Question::class => QuestionPolicy::class,
     ];
 
     /**
@@ -23,14 +25,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->registerPolicies();
-
-        \Gate::define('update-question', function($user, $question){
-            return $user->id === $question->user_id;
-        });
-
-        \Gate::define('delete-question', function($user, $question){
-            return $user->id === $question->user_id;
-        });
+        //
     }
 }
