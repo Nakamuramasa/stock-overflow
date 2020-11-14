50358b2906e4dade47585e67ddd2b2c49113cff7
Nakamuramasa <ebrten14@gmail.com>
2020-11-15 00:38:55 +0900

    Create a basic vue js component

---
+++ resources/js/components/UserInfo.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <span class="text-muted">{{ postDate }}</span>
+        <div class="media mt-2">
+            <a :href="user.url" class="pr-2">
+                <img :src="user.avatar">
+            </a>
+            <div class="media-body mt-1">
+                <a :href="user.url">{{ model.user.name }}</a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            user: this.model.user,
+        }
+    },
+    props: ['model', 'label'],
+    computed: {
+        postDate(){
+            return this.label + " " + this.model.created_date;
+        }
+    },
+}
+</script>
