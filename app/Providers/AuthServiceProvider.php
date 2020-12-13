4e34b64742196ac474134ca4ac7bda4c71144280
Nakamuramasa <ebrten14@gmail.com>
2020-12-13 22:36:03 +0900

    Install & configure Laravel for API Authentication

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Answer;
 use App\Question;
 use App\Policies\AnswerPolicy;
+use Laravel\Passport\Passport;
 use App\Policies\QuestionPolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -28,6 +29,6 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Passport::routes();
     }
 }
